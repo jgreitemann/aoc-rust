895b1da8fdd93f92424346cb112d30c624b3cd9c
Jonas Greitemann <jgreitemann@gmail.com>
2022-10-31 22:32:21 +0100

    Implement part 2 of day 7 of AoC 2017

--- aoc_2017/src/day07.rs
+++ aoc_2017/src/day07.rs
@@ -1,5 +1,5 @@
 use aoc_companion::prelude::*;
-use itertools::Itertools;
+use itertools::{Itertools, MinMaxResult};
 use std::{
     collections::{HashMap, HashSet},
     fmt::Display,
@@ -17,18 +17,25 @@ pub enum GraphError {
     GraphIsCyclic,
     #[error("There are multiple tree components")]
     MultipleTreeComponents,
+    #[error("The tower is fully balanced")]
+    TowerIsBalanced,
+    #[error("The minority subtower weight ({minority_weight}) was found {minority_count} times")]
+    AmbiguousMinorityWeight {
+        minority_weight: u32,
+        minority_count: usize,
+    },
 }
 
 pub struct Door<'input> {
-    forward_relations: HashMap<ProgramName<'input>, ForwardRelation<'input>>,
+    relations: HashMap<ProgramName<'input>, Relation<'input>>,
 }
 
 impl<'input> ParseInput<'input> for Door<'input> {
     type Error = ParseError;
 
     fn parse(input: &'input str) -> Result<Self, Self::Error> {
-        let forward_relations = parse_input(input)?;
-        Ok(Self { forward_relations })
+        let relations = parse_input(input)?;
+        Ok(Self { relations })
     }
 }
 
@@ -37,11 +44,21 @@ impl<'input> Part1 for Door<'input> {
     type Error = GraphError;
 
     fn part1(&self) -> Result<Self::Output, Self::Error> {
-        find_bottom_program(&self.forward_relations)
+        find_bottom_program(&self.relations)
     }
 }
 
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+impl Part2 for Door<'_> {
+    type Output = u32;
+    type Error = GraphError;
+
+    fn part2(&self) -> Result<Self::Output, Self::Error> {
+        let subtower_weights = calc_subtower_weights(&self.relations)?;
+        correct_weight(&self.relations, &subtower_weights)
+    }
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct ProgramName<'input>(&'input str);
 
 impl Display for ProgramName<'_> {
@@ -51,14 +68,14 @@ impl Display for ProgramName<'_> {
 }
 
 #[derive(Debug)]
-struct ForwardRelation<'input> {
+struct Relation<'input> {
     weight: u32,
     decendents: HashSet<ProgramName<'input>>,
 }
 
 fn parse_input<'input>(
     input: &'input str,
-) -> Result<HashMap<ProgramName<'input>, ForwardRelation<'input>>, ParseError> {
+) -> Result<HashMap<ProgramName<'input>, Relation<'input>>, ParseError> {
     let re = regex::Regex::new(
         r"^(?P<prog>\w+) \((?P<weight>\d+)\)(?: -> (?P<decendents>(?:\w+)(?:, (?:\w+))*))?$",
     )
@@ -75,7 +92,7 @@ fn parse_input<'input>(
         .map_ok(|caps| {
             (
                 ProgramName(caps.name("prog").unwrap().as_str()),
-                ForwardRelation {
+                Relation {
                     weight: caps["weight"].parse().unwrap(),
                     decendents: caps
                         .name("decendents")
@@ -94,14 +111,14 @@ fn parse_input<'input>(
 }
 
 fn find_bottom_program<'input>(
-    forward_relations: &HashMap<ProgramName<'input>, ForwardRelation<'input>>,
+    relations: &HashMap<ProgramName<'input>, Relation<'input>>,
 ) -> Result<ProgramName<'input>, GraphError> {
-    let dependent_programs: HashSet<ProgramName> = forward_relations
+    let dependent_programs: HashSet<ProgramName> = relations
         .values()
         .flat_map(|r| r.decendents.iter())
         .cloned()
         .collect();
-    forward_relations
+    relations
         .keys()
         .filter(|k| !dependent_programs.contains(k))
         .cloned()
@@ -110,10 +127,136 @@ fn find_bottom_program<'input>(
         .ok_or(GraphError::GraphIsCyclic)
 }
 
+fn tuple_op<A1, B1, C1, A2, B2, C2>(
+    f1: impl Fn(A1, B1) -> C1,
+    f2: impl Fn(A2, B2) -> C2,
+) -> impl Fn((A1, A2), (B1, B2)) -> (C1, C2) {
+    move |(a1, a2), (b1, b2)| (f1(a1, b1), f2(a2, b2))
+}
+
+fn merge<K, V>(lhs: HashMap<K, V>, rhs: HashMap<K, V>) -> HashMap<K, V>
+where
+    K: Eq + std::hash::Hash,
+{
+    let (mut source, mut sink) = if lhs.len() < rhs.len() {
+        (lhs, rhs)
+    } else {
+        (rhs, lhs)
+    };
+    for (k, v) in source.drain() {
+        sink.insert(k, v);
+    }
+    sink
+}
+
+fn subtower_weights_impl<'input>(
+    relations: &HashMap<ProgramName<'input>, Relation<'input>>,
+    bottom_program: ProgramName<'input>,
+) -> (u32, HashMap<ProgramName<'input>, u32>) {
+    let Relation { weight, decendents } = &relations[&bottom_program];
+    let (subtower_weight, mut map) = decendents
+        .iter()
+        .map(|&prog| subtower_weights_impl(relations, prog))
+        .fold(
+            (*weight, HashMap::new()),
+            tuple_op(std::ops::Add::add, merge),
+        );
+    map.insert(bottom_program, subtower_weight);
+    (subtower_weight, map)
+}
+
+fn calc_subtower_weights<'input>(
+    relations: &HashMap<ProgramName<'input>, Relation<'input>>,
+) -> Result<HashMap<ProgramName<'input>, u32>, GraphError> {
+    Ok(subtower_weights_impl(relations, find_bottom_program(relations)?).1)
+}
+
+struct SubtowerIter<'input, 'rel> {
+    relations: &'rel HashMap<ProgramName<'input>, Relation<'input>>,
+    current: ProgramName<'input>,
+    iter: std::collections::hash_set::Iter<'rel, ProgramName<'input>>,
+    subiter: Option<Box<dyn Iterator<Item = ProgramName<'input>> + 'rel>>,
+}
+
+impl<'input, 'rel> SubtowerIter<'input, 'rel> {
+    fn new(
+        relations: &'rel HashMap<ProgramName<'input>, Relation<'input>>,
+        current: ProgramName<'input>,
+    ) -> Self {
+        let iter = relations[&current].decendents.iter();
+        Self {
+            relations,
+            current,
+            iter,
+            subiter: None,
+        }
+    }
+}
+
+impl<'input> Iterator for SubtowerIter<'input, '_> {
+    type Item = ProgramName<'input>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match &mut self.subiter {
+            None => self.iter.next().cloned().or_else(|| {
+                self.subiter = Some(Box::new(
+                    self.relations[&self.current]
+                        .decendents
+                        .iter()
+                        .map(|desc| SubtowerIter::new(self.relations, *desc))
+                        .flatten(),
+                ));
+                self.next()
+            }),
+            Some(sub) => sub.next(),
+        }
+    }
+}
+
+fn correct_weight(
+    relations: &HashMap<ProgramName, Relation>,
+    subtower_weights: &HashMap<ProgramName, u32>,
+) -> Result<u32, GraphError> {
+    let root = find_bottom_program(relations)?;
+
+    let subtowers = std::iter::once(root).chain(SubtowerIter::new(relations, root));
+
+    let (weights, minority_weight, minority_count, majority_weight) = subtowers
+        .map(|prog| {
+            let Relation { decendents, .. } = &relations[&prog];
+            (
+                decendents
+                    .iter()
+                    .map(|desc| (subtower_weights[desc], relations[desc].weight))
+                    .collect::<HashMap<_, _>>(),
+                decendents.iter().counts_by(|desc| subtower_weights[desc]),
+            )
+        })
+        .filter_map(|(weights, counts)| {
+            match counts.into_iter().minmax_by_key(|(_, count)| *count) {
+                MinMaxResult::NoElements | MinMaxResult::OneElement(_) => None,
+                MinMaxResult::MinMax((minority_weight, minority_count), (majority_weight, _)) => {
+                    Some((weights, minority_weight, minority_count, majority_weight))
+                }
+            }
+        })
+        .last()
+        .ok_or(GraphError::TowerIsBalanced)?;
+
+    match minority_count {
+        1 => Ok(weights[&minority_weight] + majority_weight - minority_weight),
+        _ => Err(GraphError::AmbiguousMinorityWeight {
+            minority_weight,
+            minority_count,
+        }),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use assert_matches::assert_matches;
+    use itertools::assert_equal;
     use rstest::*;
 
     const EXAMPLE_INPUT: &str = r#"pbga (66)
@@ -133,7 +276,7 @@ cntj (57)
 
     #[fixture]
     #[once]
-    fn forward_relations() -> HashMap<ProgramName<'static>, ForwardRelation<'static>> {
+    fn relations() -> HashMap<ProgramName<'static>, Relation<'static>> {
         parse_input(EXAMPLE_INPUT).unwrap()
     }
 
@@ -152,7 +295,7 @@ cntj (57)
     #[case("gyxo", 61, &[])]
     #[case("cntj", 57, &[])]
     fn valid_input_is_parsed(
-        forward_relations: &HashMap<ProgramName, ForwardRelation>,
+        relations: &HashMap<ProgramName, Relation>,
         #[case] prog: &str,
         #[case] exp_weight: u32,
         #[case] names: &[&str],
@@ -161,8 +304,8 @@ cntj (57)
             names.len() == d.len() && names.into_iter().all(|name| d.contains(&ProgramName(name)))
         };
         assert_matches!(
-                forward_relations.get(&ProgramName(prog)),
-                Some(ForwardRelation{ weight, decendents })
+                relations.get(&ProgramName(prog)),
+                Some(Relation{ weight, decendents })
                 if *weight == exp_weight && matches(decendents, names)
         );
     }
@@ -178,40 +321,121 @@ cntj (57)
     }
 
     #[rstest]
-    fn bottom_program_is_found(forward_relations: &HashMap<ProgramName, ForwardRelation>) {
-        assert_matches!(
-            find_bottom_program(forward_relations),
-            Ok(ProgramName("tknk"))
-        );
+    fn bottom_program_is_found(relations: &HashMap<ProgramName, Relation>) {
+        assert_matches!(find_bottom_program(relations), Ok(ProgramName("tknk")));
     }
 
     #[test]
     fn bottom_program_does_not_exist_because_of_cycles() {
-        let forward_relations = parse_input(
-            r#"A (42) -> B
-B (42) -> C
-C (42) -> A
-"#,
-        )
-        .unwrap();
+        let relations = HashMap::from([
+            (
+                ProgramName("A"),
+                Relation {
+                    weight: 42,
+                    decendents: HashSet::from([ProgramName("B")]),
+                },
+            ),
+            (
+                ProgramName("B"),
+                Relation {
+                    weight: 42,
+                    decendents: HashSet::from([ProgramName("C")]),
+                },
+            ),
+            (
+                ProgramName("C"),
+                Relation {
+                    weight: 42,
+                    decendents: HashSet::from([ProgramName("A")]),
+                },
+            ),
+        ]);
         assert_matches!(
-            find_bottom_program(&forward_relations),
+            find_bottom_program(&relations),
             Err(GraphError::GraphIsCyclic)
         );
     }
 
     #[test]
     fn bottom_program_is_ambiguous_because_graph_has_multiple_components() {
-        let forward_relations = parse_input(
-            r#"A (42) -> B
-B (42) -> C
-D (42) -> E
-"#,
-        )
-        .unwrap();
+        let relations = HashMap::from([
+            (
+                ProgramName("A"),
+                Relation {
+                    weight: 42,
+                    decendents: HashSet::from([ProgramName("B")]),
+                },
+            ),
+            (
+                ProgramName("B"),
+                Relation {
+                    weight: 42,
+                    decendents: HashSet::from([ProgramName("C")]),
+                },
+            ),
+            (
+                ProgramName("D"),
+                Relation {
+                    weight: 42,
+                    decendents: HashSet::from([ProgramName("E")]),
+                },
+            ),
+        ]);
         assert_matches!(
-            find_bottom_program(&forward_relations),
+            find_bottom_program(&relations),
             Err(GraphError::MultipleTreeComponents)
         );
+    }
+
+    #[fixture]
+    #[once]
+    fn subtower_weights(
+        relations: &HashMap<ProgramName<'static>, Relation<'static>>,
+    ) -> HashMap<ProgramName<'static>, u32> {
+        calc_subtower_weights(relations).unwrap()
+    }
+
+    #[rstest]
+    #[case(ProgramName("gyxo"), 61)]
+    #[case(ProgramName("jptl"), 61)]
+    #[case(ProgramName("ebii"), 61)]
+    #[case(ProgramName("pbga"), 66)]
+    #[case(ProgramName("havc"), 66)]
+    #[case(ProgramName("qoyq"), 66)]
+    #[case(ProgramName("ktlj"), 57)]
+    #[case(ProgramName("cntj"), 57)]
+    #[case(ProgramName("xhth"), 57)]
+    #[case(ProgramName("ugml"), 251)]
+    #[case(ProgramName("padx"), 243)]
+    #[case(ProgramName("fwft"), 243)]
+    #[case(ProgramName("tknk"), 778)]
+    fn subtower_weights_are_determined(
+        subtower_weights: &HashMap<ProgramName, u32>,
+        #[case] prog: ProgramName,
+        #[case] exp_weight: u32,
+    ) {
+        assert_eq!(subtower_weights.get(&prog), Some(&exp_weight));
+    }
+
+    #[rstest]
+    fn subtowers_are_iterated_in_order(relations: &HashMap<ProgramName, Relation>) {
+        assert_equal(
+            SubtowerIter::new(relations, ProgramName("tknk"))
+                .take(3)
+                .sorted(),
+            [
+                ProgramName("fwft"),
+                ProgramName("padx"),
+                ProgramName("ugml"),
+            ],
+        );
+    }
+
+    #[rstest]
+    fn corrected_weight_is_determined(
+        relations: &HashMap<ProgramName, Relation>,
+        subtower_weights: &HashMap<ProgramName, u32>,
+    ) {
+        assert_matches!(correct_weight(relations, subtower_weights), Ok(60));
     }
 }
