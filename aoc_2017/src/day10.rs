d8c0dcf48d66e87f78fbc6a73de4c41b28f19409
Jonas Greitemann <jgreitemann@gmail.com>
2022-11-23 23:45:57 +0100

    Implement part 2 of day 10 of AoC 2017

--- aoc_2017/src/day10.rs
+++ aoc_2017/src/day10.rs
@@ -14,7 +14,7 @@ impl<'input> ParseInput<'input> for Door<'input> {
     type Error = std::convert::Infallible;
 
     fn parse(input: &'input str) -> Result<Self, Self::Error> {
-        Ok(Self { input })
+        Ok(Self { input: input.trim() })
     }
 }
 
@@ -36,8 +36,17 @@ impl Part1 for Door<'_> {
     }
 }
 
+impl Part2 for Door<'_> {
+    type Output = String;
+    type Error = std::convert::Infallible;
+
+    fn part2(&self) -> Result<Self::Output, Self::Error> {
+        Ok(knot_hash(&self.input))
+    }
+}
+
 fn as_list_of_numbers(input: &str) -> Result<Vec<u8>, ParseIntError> {
-    input.trim().split(',').map(str::parse).try_collect()
+    input.split(',').map(str::parse).try_collect()
 }
 
 fn apply_ties<const N: usize>(lengths: impl Iterator<Item = u8>) -> KnotState<N> {
@@ -51,6 +60,34 @@ fn product_of_first_two_elements<const N: usize>(state: &KnotState<N>) -> Result
         [first, second, ..] => Ok(u16::overflowing_mul(first as u16, second as u16).0),
         _ => Err(Error::NotEnoughElements),
     }
+}
+
+fn sparse_hash<const N: usize>(input: &str) -> [u8; N] {
+    let lengths = itertools::repeat_n(
+        input
+            .as_bytes()
+            .into_iter()
+            .copied()
+            .chain([17u8, 31u8, 73u8, 47u8, 23u8].into_iter()),
+        64,
+    )
+    .flatten();
+
+    apply_ties(lengths).marks
+}
+
+fn densify(sparse_hash: &[u8]) -> impl Iterator<Item = u8> + '_ {
+    sparse_hash.chunks(16).map(|chunk| {
+        chunk
+        .iter()
+        .copied()
+        .reduce(std::ops::BitXor::bitxor)
+        .unwrap()
+    })
+}
+
+fn knot_hash(input: &str) -> String {
+    format!("{:02x}", densify(&sparse_hash::<256>(input)).format(""))
 }
 
 struct KnotState<const N: usize> {
@@ -111,5 +148,21 @@ mod tests {
                 .unwrap(),
             12
         );
+    }
+
+    #[test]
+    fn sparse_hash_is_densified() {
+        assert_equal(
+            densify(&[65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]),
+            [64],
+        );
+    }
+
+    #[test]
+    fn example_knot_hashes() {
+        assert_eq!(knot_hash(""), "a2582a3a0e66e6e86e3812dcb672a272");
+        assert_eq!(knot_hash("AoC 2017"), "33efeb34ea91902bb2f59c9920caa6cd");
+        assert_eq!(knot_hash("1,2,3"), "3efbe78a8d82f29979031a4aa0b16a9d");
+        assert_eq!(knot_hash("1,2,4"), "63960835bcdc130f0b66d7ff4f6a5a8e");
     }
 }
