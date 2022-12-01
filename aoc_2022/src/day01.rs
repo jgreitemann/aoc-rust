1b176fbefbac17a7535595a3943e1d1928d86ef5
Jonas Greitemann <jgreitemann@gmail.com>
2022-12-01 08:01:25 +0100

    Day 1 of AoC 2022, parts 1 & 2

---
+++ aoc_2022/src/day01.rs
@@ -0,0 +1,93 @@
+use aoc_companion::prelude::*;
+
+use itertools::Itertools;
+
+pub struct Door {
+    elves: Vec<Vec<u32>>,
+}
+
+impl ParseInput<'_> for Door {
+    type Error = std::convert::Infallible;
+
+    fn parse(input: &str) -> Result<Self, Self::Error> {
+        Ok(Self{ elves: parse_input(input) })
+    }
+}
+
+impl Part1 for Door {
+    type Output = u32;
+    type Error = std::convert::Infallible;
+
+    fn part1(&self) -> Result<Self::Output, Self::Error> {
+        Ok(calories_of_top_n(&self.elves, 1))
+    }
+}
+
+impl Part2 for Door {
+    type Output = u32;
+    type Error = std::convert::Infallible;
+
+    fn part2(&self) -> Result<Self::Output, Self::Error> {
+        Ok(calories_of_top_n(&self.elves, 3))
+    }
+}
+
+fn parse_input(input: &str) -> Vec<Vec<u32>>
+{
+    input.lines()
+    .map(|line| line.parse())
+    .group_by(|line| line.is_ok())
+    .into_iter()
+    .filter_map(|(is_number, group)| if is_number { Some(group.map(Result::unwrap).collect()) } else { None })
+    .collect()
+}
+
+fn calories_of_top_n<E: AsRef<[u32]>>(elves: &[E], n: usize) -> u32 {
+    elves.into_iter().map(|items| items.as_ref().into_iter().sum())
+    .sorted_by(|lhs, rhs| <u32 as Ord>::cmp(&rhs, &lhs))
+    .take(n).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use itertools::assert_equal;
+
+    const EXAMPLE_INPUT: &str = r"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    const EXAMPLE_ELVES: &[&[u32]] = &[
+        &[1000, 2000, 3000],
+        &[4000],
+        &[5000, 6000],
+        &[7000, 8000, 9000],
+        &[10000],
+    ];
+
+    #[test]
+    fn input_is_parsed() {
+        assert_equal(parse_input(EXAMPLE_INPUT).iter(), EXAMPLE_ELVES);
+    }
+
+    #[test]
+    fn elf_with_most_calories_is_found() {
+        assert_eq!(calories_of_top_n(EXAMPLE_ELVES, 1), 24000);
+    }
+
+    #[test]
+    fn calories_of_top_3_elves() {
+        assert_eq!(calories_of_top_n(EXAMPLE_ELVES, 3), 45000);
+    }
+}
--- aoc_2022/src/main.rs
+++ aoc_2022/src/main.rs
@@ -1,8 +1,11 @@
+mod day01;
+
 use aoc_companion::prelude::*;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     aoc_main(&[
+        door!(DoorDate { day: 1, year: 2022 }, day01::Door),
         // append "doors" here...
     ])
     .await
