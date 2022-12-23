17f3ad5d8d746deb680f72223606f54ddf1ac972
Jonas Greitemann <jgreitemann@gmail.com>
2022-12-23 12:16:54 +0100

    Day 21: Fix release test failure due to unstable function pointers

--- aoc_2022/src/day21.rs
+++ aoc_2022/src/day21.rs
@@ -54,8 +54,65 @@ pub enum ParseError {
 pub enum RuntimeError {
     #[error("Could not find an equation involving {0:?}")]
     CouldNotFindAnEquationInvolvingVariable(String),
-    #[error("Operation cannot be inverted")]
-    NoInversion,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Operator {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    SubInv,
+    DivInv,
+}
+
+impl Operator {
+    fn apply(&self, lhs: isize, rhs: isize) -> isize {
+        match self {
+            Operator::Add => lhs + rhs,
+            Operator::Sub => lhs - rhs,
+            Operator::Mul => lhs * rhs,
+            Operator::Div => lhs / rhs,
+            Operator::SubInv => rhs - lhs,
+            Operator::DivInv => rhs / lhs,
+        }
+    }
+
+    fn inv_lhs(&self) -> Self {
+        // t = x + a => x = t - a
+        // t = x - a => x = t + a
+        // t = x * a => x = t / a
+        // t = x / a => x = t * a
+        // t = a - x => x = a - t
+        // t = a / x => x = a / t
+        use Operator::*;
+        match *self {
+            Add => Sub,
+            Sub => Add,
+            Mul => Div,
+            Div => Mul,
+            SubInv => SubInv,
+            DivInv => DivInv,
+        }
+    }
+
+    fn inv_rhs(&self) -> Self {
+        // t = a + x => x = t - a
+        // t = a - x => x = a - t
+        // t = a * x => x = t / a
+        // t = a / x => x = a / t
+        // t = x - a => x = t + a
+        // t = x / a => x = t * a
+        use Operator::*;
+        match *self {
+            Add => Sub,
+            Sub => SubInv,
+            Mul => Div,
+            Div => DivInv,
+            SubInv => Add,
+            DivInv => Mul,
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -64,7 +121,7 @@ enum Monkey {
     Operation {
         lhs: String,
         rhs: String,
-        ops: fn(isize, isize) -> isize,
+        ops: Operator,
     },
 }
 
@@ -81,7 +138,7 @@ impl Monkey {
                 (Some(_), None) => Resolution::Dependent(vec![rhs.clone()]),
                 (None, Some(_)) => Resolution::Dependent(vec![lhs.clone()]),
                 (None, None) => Resolution::Dependent(vec![lhs.clone(), rhs.clone()]),
-                (Some(&lhs), Some(&rhs)) => Resolution::Ready(ops(lhs, rhs)),
+                (Some(&lhs), Some(&rhs)) => Resolution::Ready(ops.apply(lhs, rhs)),
             },
         }
     }
@@ -97,10 +154,10 @@ impl FromStr for Monkey {
                 .split_once(' ')
                 .ok_or_else(|| ParseError::OperatorExpressionNotFound(rest.to_string()))?;
             let ops = match ops_str {
-                "+" => Ok(std::ops::Add::add as fn(isize, isize) -> isize),
-                "-" => Ok(std::ops::Sub::sub as fn(isize, isize) -> isize),
-                "*" => Ok(std::ops::Mul::mul as fn(isize, isize) -> isize),
-                "/" => Ok(std::ops::Div::div as fn(isize, isize) -> isize),
+                "+" => Ok(Operator::Add),
+                "-" => Ok(Operator::Sub),
+                "*" => Ok(Operator::Mul),
+                "/" => Ok(Operator::Div),
                 _ => Err(ParseError::UnrecognizedOperator(ops_str.to_owned())),
             }?;
             Ok(Monkey::Operation {
@@ -150,34 +207,6 @@ fn solve_for(target: &str, monkeys: &HashMap<String, Monkey>) -> Result<isize, R
     Ok(data[target])
 }
 
-// t = x + a => x = t - a
-// t = x - a => x = t + a
-// t = x * a => x = t / a
-// t = x / a => x = t * a
-fn inv_lhs(f: fn(isize, isize) -> isize) -> Result<fn(isize, isize) -> isize, RuntimeError> {
-    match f {
-        f if f == std::ops::Add::add => Ok(std::ops::Sub::sub),
-        f if f == std::ops::Sub::sub => Ok(std::ops::Add::add),
-        f if f == std::ops::Mul::mul => Ok(std::ops::Div::div),
-        f if f == std::ops::Div::div => Ok(std::ops::Mul::mul),
-        _ => Err(RuntimeError::NoInversion),
-    }
-}
-
-// t = a + x => x = t - a
-// t = a - x => x = a - t
-// t = a * x => x = t / a
-// t = a / x => x = a / t
-fn inv_rhs(f: fn(isize, isize) -> isize) -> Result<fn(isize, isize) -> isize, RuntimeError> {
-    match f {
-        f if f == std::ops::Add::add => Ok(std::ops::Sub::sub),
-        f if f == std::ops::Sub::sub => Ok(|t, a| a - t),
-        f if f == std::ops::Mul::mul => Ok(std::ops::Div::div),
-        f if f == std::ops::Div::div => Ok(|t, a| a / t),
-        _ => Err(RuntimeError::NoInversion),
-    }
-}
-
 fn transform_for_part2(
     monkeys: &HashMap<String, Monkey>,
 ) -> Result<HashMap<String, Monkey>, RuntimeError> {
@@ -188,10 +217,10 @@ fn transform_for_part2(
             .iter()
             .find_map(|(name, monkey)| match monkey {
                 Monkey::Operation { lhs, rhs, ops } if lhs == &current => {
-                    Some((name, rhs, inv_lhs(*ops)))
+                    Some((name, rhs, ops.inv_lhs()))
                 }
                 Monkey::Operation { lhs, rhs, ops } if rhs == &current => {
-                    Some((name, lhs, inv_rhs(*ops)))
+                    Some((name, lhs, ops.inv_rhs()))
                 }
                 _ => None,
             })
@@ -202,7 +231,7 @@ fn transform_for_part2(
         let new = Monkey::Operation {
             lhs: name.clone(),
             rhs: other.clone(),
-            ops: ops?,
+            ops,
         };
         let next = name.clone();
         new_monkeys.remove(&next);
@@ -299,7 +328,7 @@ cczh: sjmn * lfqf";
                 Monkey::Operation {
                     lhs: "pppw".to_owned(),
                     rhs: "sjmn".to_owned(),
-                    ops: std::ops::Add::add,
+                    ops: Operator::Add,
                 },
             ),
             ("dbpl".to_owned(), Monkey::Constant(5)),
@@ -308,7 +337,7 @@ cczh: sjmn * lfqf";
                 Monkey::Operation {
                     lhs: "sllz".to_owned(),
                     rhs: "lgvd".to_owned(),
-                    ops: std::ops::Add::add,
+                    ops: Operator::Add,
                 },
             ),
             ("zczc".to_owned(), Monkey::Constant(2)),
@@ -317,7 +346,7 @@ cczh: sjmn * lfqf";
                 Monkey::Operation {
                     lhs: "humn".to_owned(),
                     rhs: "dvpt".to_owned(),
-                    ops: std::ops::Sub::sub,
+                    ops: Operator::Sub,
                 },
             ),
             ("dvpt".to_owned(), Monkey::Constant(3)),
@@ -329,7 +358,7 @@ cczh: sjmn * lfqf";
                 Monkey::Operation {
                     lhs: "drzm".to_owned(),
                     rhs: "dbpl".to_owned(),
-                    ops: std::ops::Mul::mul,
+                    ops: Operator::Mul,
                 },
             ),
             ("sllz".to_owned(), Monkey::Constant(4)),
@@ -338,7 +367,7 @@ cczh: sjmn * lfqf";
                 Monkey::Operation {
                     lhs: "cczh".to_owned(),
                     rhs: "lfqf".to_owned(),
-                    ops: std::ops::Div::div,
+                    ops: Operator::Div,
                 },
             ),
             (
@@ -346,7 +375,7 @@ cczh: sjmn * lfqf";
                 Monkey::Operation {
                     lhs: "ljgn".to_owned(),
                     rhs: "ptdq".to_owned(),
-                    ops: std::ops::Mul::mul,
+                    ops: Operator::Mul,
                 },
             ),
             (
@@ -354,7 +383,7 @@ cczh: sjmn * lfqf";
                 Monkey::Operation {
                     lhs: "hmdt".to_owned(),
                     rhs: "zczc".to_owned(),
-                    ops: std::ops::Sub::sub,
+                    ops: Operator::Sub,
                 },
             ),
             ("hmdt".to_owned(), Monkey::Constant(32)),
