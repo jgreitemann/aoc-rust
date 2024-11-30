79241c2880622d984d9df9b84e57cf285c6957cf
Jonas Greitemann <jgreitemann@gmail.com>
2024-11-30 20:34:31 +0100

    feat: Create executable for AoC 2024

---
+++ aoc_2024/src/main.rs
@@ -0,0 +1,11 @@
+#![allow(refining_impl_trait_internal)]
+
+use aoc_companion::prelude::*;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    aoc_main(&[
+        // append "doors" here...
+    ])
+    .await
+}
