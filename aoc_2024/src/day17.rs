105d8b945ebb5759ba77e84e027cabcadfc3d935
Jonas Greitemann <jgreitemann@gmail.com>
2024-12-18 21:06:24 +0100

    feat: part 2 of day 17 of AoC 2024

--- aoc_2024/src/day17.rs
+++ aoc_2024/src/day17.rs
@@ -1,7 +1,7 @@
 use anyhow::{anyhow, bail};
 use aoc_companion::prelude::*;
 use aoc_utils::array;
-use itertools::Itertools;
+use itertools::{iterate, Itertools};
 
 pub(crate) struct Door {
     initial_registers: [u64; 3],
@@ -45,6 +45,21 @@ impl<'input> Solution<'input> for Door {
 
     fn part1(&self) -> String {
         self.run().join(",")
+    }
+
+    fn part2(&self) -> Result<u64> {
+        let a = reversed_self_replication_value(&self.program);
+        let test_computer = Computer {
+            ip: 0,
+            registers: [a, 0, 0],
+            program: &self.program,
+        };
+
+        if test_computer.eq(self.program.iter().map(|&x| x as u64)) {
+            Ok(a)
+        } else {
+            Err(anyhow!("self-consistency check failed"))
+        }
     }
 }
 
@@ -132,9 +147,49 @@ fn brute_force_self_replication_value(program: &[u8]) -> u64 {
         .unwrap()
 }
 
+#[allow(dead_code)]
+fn run_reverse_engineered_program(initial_registers: [u64; 3]) -> impl Iterator<Item = u64> {
+    iterate(initial_registers, |&[mut a, _, _]| {
+        let mut b = a % 8; // bst a
+        b ^= 3; // bxl 3
+        let c = a >> b; // cdv b
+        b ^= c; // bxc _
+        a >>= 3; // adv 3
+        b ^= 5; // bxl 5
+        [a, b, c]
+    })
+    .skip(1)
+    .take_while_inclusive(|&[a, _, _]| a != 0) // jnz 0
+    .map(|[_, b, _]| b % 8) // out b
+}
+
+fn reversed_self_replication_value(program: &[u8]) -> u64 {
+    program
+        .iter()
+        .rev()
+        .fold(vec![0], |possible_a, b4| {
+            possible_a
+                .iter()
+                .flat_map(|a| {
+                    let b3 = *b4 as u64 ^ 5;
+                    (0..8).filter_map(move |b2| {
+                        let b1 = b2 ^ 3;
+                        let a2 = (a << 3) | b1;
+                        let c = a2 >> b2;
+                        (b3 == b2 ^ (c % 8)).then_some(a2)
+                    })
+                })
+                .collect()
+        })
+        .into_iter()
+        .min()
+        .expect("should be possible for some A")
+}
+
 #[cfg(test)]
 mod tests {
     use itertools::assert_equal;
+    use proptest::proptest;
 
     use super::*;
 
@@ -148,6 +203,7 @@ Program: 0,1,5,4,3,0";
     const EXAMPLE_INIT_REG: [u64; 3] = [729, 0, 0];
     const EXAMPLE_PROGRAM: &[u8] = &[0, 1, 5, 4, 3, 0];
     const SELF_REPL_PROGRAM: &[u8] = &[0, 3, 5, 4, 3, 0];
+    const INPUT_PROGRAM: &[u8] = &[2, 4, 1, 3, 7, 5, 4, 2, 0, 3, 1, 5, 5, 5, 3, 0];
 
     #[test]
     fn parse_example_input() {
@@ -181,6 +237,29 @@ Program: 0,1,5,4,3,0";
         assert_eq!(
             brute_force_self_replication_value(SELF_REPL_PROGRAM),
             117440
+        );
+    }
+
+    proptest! {
+
+        #[test]
+        fn reverse_engineered_function_produces_same_results(a in 0..=u64::MAX) {
+            let control_computer = Computer {
+                ip: 0,
+                registers: [a, 0, 0],
+                program: INPUT_PROGRAM,
+            };
+            assert_equal(run_reverse_engineered_program([a, 0, 0]), control_computer);
+        }
+
+    }
+
+    #[test]
+    fn reverse_engineered_self_replication_value_replicates_program() {
+        let a = reversed_self_replication_value(INPUT_PROGRAM);
+        assert_equal(
+            run_reverse_engineered_program([a, 0, 0]),
+            INPUT_PROGRAM.iter().map(|p| *p as u64),
         );
     }
 }
