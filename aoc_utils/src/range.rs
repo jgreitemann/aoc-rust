820277bf37614b608ed2f63b3b55e3dfd50fe9f1
Jonas Greitemann <jgreitemann@gmail.com>
2025-12-05 21:37:21 +0100

    feat: add `try_union` function to `RangeSet` trait

--- aoc_utils/src/range.rs
+++ aoc_utils/src/range.rs
@@ -2,6 +2,7 @@ use std::ops::RangeInclusive;
 
 /// Extension trait for set operations on range types
 pub trait RangeSet: Sized {
+    fn try_union(&self, other: &Self) -> Option<Self>;
     fn intersection(&self, other: &Self) -> Option<Self>;
 }
 
@@ -9,6 +10,16 @@ impl<T> RangeSet for RangeInclusive<T>
 where
     T: Copy + PartialOrd + Ord,
 {
+    fn try_union(&self, other: &Self) -> Option<Self> {
+        if self.contains(other.start()) {
+            Some(*self.start()..=*self.end().max(other.end()))
+        } else if other.contains(self.start()) {
+            Some(*other.start()..=*self.end().max(other.end()))
+        } else {
+            None
+        }
+    }
+
     fn intersection(&self, other: &Self) -> Option<Self> {
         let range = (*self.start().max(other.start()))..=(*self.end().min(other.end()));
         (!range.is_empty()).then_some(range)
@@ -18,6 +29,39 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn inclusive_range_try_union() {
+        // 1 2 3 4 5 6 7 8
+        // ---------
+        //     -----------
+        assert_eq!(RangeSet::try_union(&(1..=5), &(3..=8)), Some(1..=8));
+
+        // 1 2 3 4 5 6 7 8
+        //     -----------
+        // ---------
+        assert_eq!(RangeSet::try_union(&(3..=8), &(1..=5)), Some(1..=8));
+
+        // 1 2 3 4 5 6 7 8
+        // ---------------
+        //     -----
+        assert_eq!(RangeSet::try_union(&(1..=8), &(3..=5)), Some(1..=8));
+
+        // 1 2 3 4 5 6 7 8
+        //     -----
+        // ---------------
+        assert_eq!(RangeSet::try_union(&(3..=5), &(1..=8)), Some(1..=8));
+
+        // 1 2 3 4 5 6 7 8
+        // -----
+        //         -------
+        assert_eq!(RangeSet::try_union(&(1..=3), &(5..=8)), None);
+
+        // 1 2 3 4 5 6 7 8
+        //         -------
+        // -----
+        assert_eq!(RangeSet::try_union(&(5..=8), &(1..=3)), None);
+    }
 
     #[test]
     fn inclusive_range_intersection() {
