217bd8c23bce80ab6d937501ae497633a3ee91e4
Jonas Greitemann <jgreitemann@gmail.com>
2025-12-18 10:56:54 +0100

    refactor: generalize day 12 solution a bit
    Knowing that all problems can be conclusively judged irrespective of the
    actual present shapes, it is no longer necessary to restrict the
    solution to known shapes by asserting on a specific hash. All that's
    needed is to extract the areas occupied by each present.
    
    For different inputs, they might still be problems which cannot be
    conclusively judged based on the implemented rules (though I suspect
    that all inputs share this quality) but then my solution would still
    produce a proper error, rather than a wrong answer.

--- aoc_2025/src/day12.rs
+++ aoc_2025/src/day12.rs
@@ -5,6 +5,7 @@ use aoc_companion::prelude::*;
 use itertools::Itertools as _;
 
 pub(crate) struct Door {
+    areas: [usize; 6],
     problems: Vec<Problem>,
 }
 
@@ -13,15 +14,24 @@ impl<'input> Solution<'input> for Door {
         let Some((shapes, problems)) = input.rsplit_once("\n\n") else {
             bail!("could not find empty line delimiting shapes and problems");
         };
-        let shapes_hash = fxhash::hash(shapes);
-        if shapes_hash != 0x2050bd894f01430f {
-            bail!("solution only works for my input; got different hash: {shapes_hash:x}");
-        }
-        problems
-            .lines()
-            .map(str::parse)
-            .try_collect()
-            .map(|problems| Door { problems })
+        let areas = aoc_utils::array::try_from_iter_exact(shapes.split("\n\n").map(|shape| {
+            let Some((_, shape)) = shape.split_once(":\n") else {
+                bail!("missing shape introducer line ending in colon");
+            };
+            let shape = aoc_utils::geometry::parse_ascii_map(shape)
+                .with_context(|| anyhow!("failed to parse shape"))?;
+            if shape.dim() != (3, 3) {
+                bail!(
+                    "expected shapes of up to 3x3, got {}x{}",
+                    shape.dim().0,
+                    shape.dim().1
+                );
+            }
+            Ok(shape.iter().filter(|b| **b == b'#').count())
+        }))?
+        .map_err(|v| anyhow!("expected exactly 6 present shapes, got {}", v.len()))?;
+        let problems = problems.lines().map(str::parse).try_collect()?;
+        Ok(Door { areas, problems })
     }
 
     fn part1(&self) -> Result<usize> {
@@ -29,7 +39,7 @@ impl<'input> Solution<'input> for Door {
             .problems
             .iter()
             .map(|problem| {
-                rule_out_due_to_insufficient_area(problem)
+                rule_out_due_to_insufficient_area(problem, &self.areas)
                     .or_else(|| verify_with_trivial_packing(problem))
                     .ok_or(problem)
             })
@@ -98,13 +108,11 @@ impl Problem {
     }
 }
 
-fn rule_out_due_to_insufficient_area(problem: &Problem) -> Option<bool> {
-    const AREA: [usize; 6] = [5, 7, 7, 7, 6, 7];
-
+fn rule_out_due_to_insufficient_area(problem: &Problem, areas: &[usize; 6]) -> Option<bool> {
     let required_area: usize = problem
         .presents
         .iter()
-        .zip(AREA)
+        .zip(areas)
         .map(|(count, area)| count * area)
         .sum();
 
@@ -117,4 +125,85 @@ fn verify_with_trivial_packing(problem: &Problem) -> Option<bool> {
     let total_presents: usize = problem.presents.iter().sum();
 
     (total_presents <= available_cells).then_some(true)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT: &str = "\
+0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2";
+
+    const EXAMPLE_AREAS: [usize; 6] = [7, 7, 7, 7, 7, 7];
+    const EXAMPLE_PROBLEMS: &[Problem] = &[
+        Problem {
+            dimensions: [4, 4],
+            presents: [0, 0, 0, 0, 2, 0],
+        },
+        Problem {
+            dimensions: [12, 5],
+            presents: [1, 0, 1, 0, 2, 2],
+        },
+        Problem {
+            dimensions: [12, 5],
+            presents: [1, 0, 1, 0, 3, 2],
+        },
+    ];
+
+    #[test]
+    fn parse_example_input() {
+        let Door { areas, problems } = Door::parse(EXAMPLE_INPUT).unwrap();
+        assert_eq!(areas, EXAMPLE_AREAS);
+        itertools::assert_equal(&problems, EXAMPLE_PROBLEMS);
+    }
+
+    #[test]
+    fn none_of_the_example_problems_can_be_ruled_out_due_to_insufficient_area() {
+        itertools::assert_equal(
+            EXAMPLE_PROBLEMS
+                .iter()
+                .map(|problem| rule_out_due_to_insufficient_area(problem, &EXAMPLE_AREAS)),
+            [None, None, None],
+        );
+    }
+
+    #[test]
+    fn none_of_the_example_problems_can_be_verified_with_trivial_packing() {
+        itertools::assert_equal(
+            EXAMPLE_PROBLEMS.iter().map(verify_with_trivial_packing),
+            [None, None, None],
+        );
+    }
 }
